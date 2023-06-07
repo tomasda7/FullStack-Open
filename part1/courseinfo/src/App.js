a97f8b5360f9c80156045dcf6bba3d738f702250
tomasda7 <tomasda7@outlook.com>
2023-06-07 14:40:32 -0300

    Done exercise 2 of part1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -6,12 +6,22 @@ const Header = ({course}) => {
   )
 }
 
-const Content = ({part, exercises}) => {
+const Part = ({part, exercises}) => {
   return (
     <>
       <p>
         {part}: {exercises}
-      </p>
+     </p>
+    </>
+  )
+}
+
+const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
+  return (
+    <>
+      <Part part={part1} exercises={exercises1}/>
+      <Part part={part2} exercises={exercises2}/>
+      <Part part={part3} exercises={exercises3}/>
     </>
   )
 }
@@ -35,11 +45,17 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course}/>
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Header course={course} />
+      <Content
+      part1={part1} exercises1={exercises1}
+      part2={part2} exercises2={exercises2}
+      part3={part3} exercises3={exercises3}
+      />
+      <Total
+      exercises1={exercises1}
+      exercises2={exercises2}
+      exercises3={exercises3}
+      />
     </div>
   )
 }
