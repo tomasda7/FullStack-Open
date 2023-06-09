438102f4a1b1f89bbddd289152b2d56e701e1fca
tomasda7 <tomasda7@outlook.com>
2023-06-09 18:31:17 -0300

    Done exercise 14 of part1

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,11 +1,28 @@
 import { useState } from 'react';
 
+const Header = ({title}) => {
+  return (
+    <>
+      <h2>{title}</h2>
+    </>
+  )
+}
+
 const Button = ({handleClick, text}) => {
   return (
     <>
       <button onClick={handleClick}>
         {text}
       </button>
+    </>
+  )
+}
+
+const Anecdote = ({anecdotes, selected, points}) => {
+  return (
+    <>
+      <p>{anecdotes[selected]}</p>
+      <p>has {points[selected]} votes</p>
     </>
   )
 }
@@ -27,6 +44,9 @@ const App = () => {
   const [selected, Setselected] = useState(0);
   const [points, setPoints] = useState(new Uint8Array(anecdotes.length))
 
+  let mostVoted = points.indexOf(Math.max(...points));
+
+
   const handleSelected = () => {
     let random = Math.floor(Math.random() * anecdotes.length);
     Setselected(random);
@@ -38,12 +58,16 @@ const App = () => {
     setPoints(copy);
   }
 
+
   return (
     <div>
-      <p>{anecdotes[selected]}</p>
-      <p>has {points[selected]} votes</p>
+      <Header title='Anecdote of the day'/>
+      <Anecdote anecdotes={anecdotes} selected={selected} points={points}/>
       <Button text='next anecdote' handleClick={handleSelected} />
       <Button text='vote' handleClick={handleVote} />
+      <Header title='Most voted anecdote'/>
+      <p>{anecdotes.filter((anec,i) => i === mostVoted)}</p>
+      <p>has {Math.max(...points)} votes</p>
     </div>
   );
 }
