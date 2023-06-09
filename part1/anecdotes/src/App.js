d6a846f78885176590f8a2b46b889492c7f9cd29
tomasda7 <tomasda7@outlook.com>
2023-06-09 17:09:50 -0300

    Done exercise 13 of part1

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -23,17 +23,27 @@ const App = () => {
     'The only way to go fast, is to go well.'
   ]
 
+
   const [selected, Setselected] = useState(0);
+  const [points, setPoints] = useState(new Uint8Array(anecdotes.length))
 
   const handleSelected = () => {
     let random = Math.floor(Math.random() * anecdotes.length);
     Setselected(random);
   }
 
+  const handleVote = () => {
+    const copy = [...points];
+    copy[selected] += 1;
+    setPoints(copy);
+  }
+
   return (
     <div>
-      <h3>{anecdotes[selected]}</h3>
-      <Button text='next anecdote' handleClick={handleSelected}/>
+      <p>{anecdotes[selected]}</p>
+      <p>has {points[selected]} votes</p>
+      <Button text='next anecdote' handleClick={handleSelected} />
+      <Button text='vote' handleClick={handleVote} />
     </div>
   );
 }
