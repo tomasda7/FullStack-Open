8c1d0a1157932270c6b84ceded39cfd42f4b4abe
tomasda7 <tomasda7@outlook.com>
2023-06-12 18:00:42 -0300

    Done exercise 5 of part2

--- part2/courseinfo2/src/App.js
+++ part2/courseinfo2/src/App.js
@@ -1,45 +1,4 @@
-
-const Header = ({ title }) => {
-  return (
-    <>
-      <h2>{title}</h2>
-    </>
-  )
-}
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name}: {part.exercises}
-    </p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <>
-      {parts.map(part => <Part key={part.id} part={part}/>)}
-    </>
-  )
-}
-
-const Total = ({ parts }) => {
-  return (
-    <>
-      <h4>total of {parts.reduce((a,c) =>  a + c.exercises,0)} exercises</h4>
-    </>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header title={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
-    </>
-  )
-}
+import Course from "./components/Course";
 
 const App = () => {
 
