12aac9a6857108e05c6a311eb7fd00a7b1f7233d
tomasda7 <tomasda7@outlook.com>
2023-06-29 15:28:35 -0300

    Done exercise 4 of part4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,8 +1,16 @@
 
 const dummy = (blogs) => {
-    return 1;
+  return 1;
 }
 
+const totalLikes = (posts) => {
+  const likes = posts.map(post => post.likes)
+
+  return likes.length ? likes.reduce((total,curr) => total + curr) : 0
+}
+
+
 module.exports = {
-    dummy
+  dummy,
+  totalLikes
 }
