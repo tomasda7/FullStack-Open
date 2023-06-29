b8da9e6ce5550752d6727f1109c79e8e2f6945c8
tomasda7 <tomasda7@outlook.com>
2023-06-29 17:19:07 -0300

    Done exercise 5 of part4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -3,14 +3,22 @@ const dummy = (blogs) => {
   return 1;
 }
 
-const totalLikes = (posts) => {
-  const likes = posts.map(post => post.likes)
+const totalLikes = (blogs) => {
+  const likes = blogs.map(blog => blog.likes)
 
   return likes.length ? likes.reduce((total,curr) => total + curr) : 0
 }
 
+const favoriteBlog = (blogs) => {
+    const mostLikes = Math.max(...blogs.map(blog => blog.likes))
 
-module.exports = {
+    return blogs.map(blog => {
+        return  { title: blog.title, author: blog.author, likes: blog.likes }
+    }).find(blog => blog.likes === mostLikes)
+}
+
+ module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
