6dd5975ef5f3aa348bb6b9cadec3b777b6ac5793
tomasda7 <tomasda7@outlook.com>
2023-06-29 14:55:29 -0300

    Done exercise 3 of part4

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -19,14 +19,13 @@ mongoose.connect(config.MONGODB_URI)
   .catch((err) => {
     error('Error in connection with MongoDB: ', err.message)
   })
-
 morgan.token('body', (request) => { return JSON.stringify(request.body) })
 
-app.use(morgan(':method :url :status :response-time ms - :body'))
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 
+app.use(morgan(':method :url :status :response-time ms - :body'))
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
