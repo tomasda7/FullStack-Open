d97aaa1d90fdc4f22b54782579552be277dc5166
tomasda7 <tomasda7@outlook.com>
2023-06-30 20:03:44 -0300

    Now done exercise 8 of part4

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,14 +1,39 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const Blog = require('../models/blog')
 
-test('blogs are returned as json', async () => {
-    await api
+const initialBlogs = [
+  {
+    title: "Test blog",
+    author: "tomasda7",
+    url: "http//www.test.com",
+    likes: 200,
+  },
+  {
+    title: "Cats blog",
+    author: "some cat owner",
+    url: "http//welovecats.com",
+    likes: 1000,
+  }
+]
+
+beforeEach(async() => {
+    await Blog.deleteMany({})
+    let blogObj = new Blog(initialBlogs[0])
+    await blogObj.save()
+    blogObj = new Blog(initialBlogs[1])
+    await blogObj.save()
+})
+
+test('the blogs are returned in the correct amount and as JSON', async () => {
+
+  const response = await api
       .get('/api/blogs')
       .expect(200)
       .expect('Content-Type', /application\/json/)
+    expect(response.body).toHaveLength(initialBlogs.length)
 })
 
 afterAll(async () => {
