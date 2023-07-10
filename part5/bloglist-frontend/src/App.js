7217b3f35b4e51efcd9415b04bd7907e6fbfb9ba
tomasda7 <tomasda7@outlook.com>
2023-07-10 20:39:56 -0300

    Done exercises 3 and 4 of part5

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -11,6 +11,11 @@ const App = () => {
   const [user, setUser] = useState(null)
   const [message, setMessage] = useState(null)
   const [isError, setIsError] = useState(false)
+  const [newBlog, setNewBlog] = useState({
+    title: '',
+    author: '',
+    url: ''
+  })
 
   useEffect( () => {
     const fetchBlogs = async () => {
@@ -40,13 +45,14 @@ const App = () => {
         'loggedUser', JSON.stringify(user)
       )
 
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
     }
     catch(error) {
       setIsError(true)
-      setMessage('Wrong Credentials')
+      setMessage(error.response.data.error)
       setTimeout(() => {
         setIsError(false)
         setMessage(null)
@@ -58,6 +64,42 @@ const App = () => {
   const handleLogout = () => {
     window.localStorage.removeItem('loggedUser')
     window.location.reload()
+  }
+
+  const handleNewBlog = (e) => {
+    const value = e.target.value
+    setNewBlog({
+      ...newBlog,
+      [e.target.name]: value
+    })
+  }
+
+  const addBlog = async (e) => {
+    e.preventDefault()
+
+    const blogObj = {
+      title: newBlog.title,
+      author: newBlog.author,
+      url: newBlog.url
+    }
+
+    try {
+      const newBlog = await blogService.create(blogObj)
+      setBlogs(blogs.concat(newBlog))
+      setNewBlog({ title: '', author: '', url: '' })
+      setMessage(`a new blog "${newBlog.title}" by ${user.username} was added successfully`)
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
+    }
+    catch(error) {
+      setIsError(true)
+      setMessage(error.response.data.error)
+      setTimeout(() => {
+        setMessage(null)
+        setIsError(false)
+      }, 5000)
+    }
   }
 
   if(user === null) {
@@ -98,6 +140,22 @@ const App = () => {
       <Notification message={message} style={isError ? 'error' : 'success'}/>
 
       <p>{user.name} logged in</p><button onClick={handleLogout}>Logout</button>
+
+      <h2>Create a new blog</h2>
+      <form onSubmit={addBlog}>
+        <div>
+          Title: <input type='text' name='title' value={newBlog.title} onChange={handleNewBlog}/>
+        </div>
+        <div>
+          Author: <input type='text' name='author' value={newBlog.author} onChange={handleNewBlog}/>
+        </div>
+        <div>
+          URL: <input type='text' name='url' value={newBlog.url} onChange={handleNewBlog}/>
+        </div>
+        <div>
+         <button type='submit'>Send</button>
+        </div>
+      </form>
 
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,10 +1,25 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+let token;
+
+const setToken = (newToken) => {
+  token = `Bearer ${newToken}`
+}
+
 const getAll = async () => {
   const request = await axios.get(baseUrl)
   return request.data;
 }
 
+const create = async (newObj) => {
+  const config = {
+    headers: { Authorization: token }
+  }
+
+  const response = await axios.post(baseUrl, newObj, config)
+  return response.data
+}
+
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { getAll }
+export default { getAll, create, setToken }
