b94bd253dd6646bd100efe09c9a15e10be11fb01
tomasda7 <tomasda7@outlook.com>
2023-08-09 20:05:20 -0300

    Done exercise 8 of part7

---
+++ part7/ultimate-hooks/src/App.js
@@ -0,0 +1,91 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  const reset = () => {
+    setValue('')
+  }
+
+  return {
+    type,
+    value,
+    onChange,
+    reset
+  }
+}
+
+const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([])
+
+  useEffect(() => {
+    const fetch = async (url) => {
+      const serverResources = await axios.get(url)
+      console.log(serverResources.data)
+      setResources(serverResources.data)
+    }
+
+    fetch(baseUrl)
+  }, [baseUrl])
+
+  const create = async (resource) => {
+    const newResource = await axios.post(`${baseUrl}`, resource)
+    setResources([...resources, newResource.data])
+  }
+
+  const service = {
+    create
+  }
+
+  return [
+    resources, service
+  ]
+}
+
+const App = () => {
+  const { reset: resetContent, ...content } = useField('text')
+  const { reset: resetName, ...name } = useField('text')
+  const { reset: resetNumber, ...number } = useField('text')
+
+  const [notes, noteService] = useResource('http://localhost:3005/notes')
+  const [persons, personService] = useResource('http://localhost:3005/persons')
+
+  const handleNoteSubmit = (event) => {
+    event.preventDefault()
+    noteService.create({ content: content.value })
+    resetContent()
+  }
+
+  const handlePersonSubmit = (event) => {
+    event.preventDefault()
+    personService.create({ name: name.value, number: number.value})
+    resetName()
+    resetNumber()
+  }
+
+  return (
+    <div>
+      <h2>notes</h2>
+      <form onSubmit={handleNoteSubmit}>
+        <input {...content} />
+        <button>create</button>
+      </form>
+      {notes.map(n => <p key={n.id}>{n.content}</p>)}
+
+      <h2>persons</h2>
+      <form onSubmit={handlePersonSubmit}>
+        name <input {...name} /> <br/>
+        number <input {...number} />
+        <button>create</button>
+      </form>
+      {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
+    </div>
+  )
+}
+
+export default App
