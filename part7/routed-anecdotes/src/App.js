924c42fc8dba33ecbc9846085937063d0da582b7
tomasda7 <tomasda7@outlook.com>
2023-08-09 15:55:49 -0300

    Done exercise 5 of part7

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -83,23 +83,30 @@ const CreateNew = ({ addNew }) => {
     navigate('/')
   }
 
+  const handleReset = () => {
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input {...content} />
+          <input { ...content } />
         </div>
         <div>
           author
-          <input {...author} />
+          <input { ...author } />
         </div>
         <div>
           url for more info
-          <input {...info} />
+          <input { ...info } />
         </div>
-        <button>create</button>
+        <button type='submit'>create</button>
+        <button type='button' onClick={() => handleReset()}>reset</button>
       </form>
     </div>
   )
