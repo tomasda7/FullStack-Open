45b55be1c6b0d7ccc3dcf8c739192a7838811186
tomasda7 <tomasda7@outlook.com>
2023-08-24 21:27:49 -0300

    Done exercise 4 of part8

--- part8/books-and-authors/index.js
+++ part8/books-and-authors/index.js
@@ -98,7 +98,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -107,7 +107,13 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (args.author) {
+        return books.filter((book) => book.author === args.author);
+      }
+
+      return books;
+    },
     allAuthors: () =>
       authors.map((author) => {
         return {
